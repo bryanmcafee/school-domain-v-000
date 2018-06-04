fcd87aa8f42a7d01ebb3466eb4db8c1ddba79f74
Bryan McAfee <brymcafee@gmail.com>
2018-06-04 15:27:04 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,15 +1,12 @@
 class School
 attr_accessor :name, :roster
-
 def initialize(name)
   @name = name
   @roster = {}
 end
-
 def grade(grade)
   @roster[grade]
 end
-
 def add_student(name, grade)
   if  @roster[grade] != nil
   @roster[grade] << name
@@ -17,11 +14,9 @@ else
   @roster[grade] = [name]
 end
 end
-
 def sort
   @roster.each do |key, value|
     value.sort!
   end
 end
-
 end
